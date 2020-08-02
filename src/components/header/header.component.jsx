43258686abfa1d63605a071c0882644ee9eb507c
akhil <akkeakhilkumar20@gmail.com>
2020-08-02 12:09:45 +0530

    added dropdown list for the button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,10 +6,13 @@ import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+
 import { ReactComponent as Logo } from '../../assests/crown.svg';
 
 import './header.style.scss';
-const Header = ({ currentUser }) =>(
+const Header = ({ currentUser,hidden }) =>(
   <div className='header'>
    <Link className='logo-container' to="/">
    <Logo className='logo'/>
@@ -35,12 +38,18 @@ const Header = ({ currentUser }) =>(
      )
     
    }
+   <CartIcon/>
    </div>
+  { 
+    hidden ? null :
+    <CartDropdown/>
+  }
   </div>
 );
-const mapStateToProps = state =>({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) =>({
+  currentUser,
+  hidden
+});
 
 
 export default connect(mapStateToProps)(Header);
